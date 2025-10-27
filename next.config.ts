afc7a337a4881e71098a55c685426684dec03ae9
Bawantha <bawanthapasqual@gmail.com>
2025-10-27 17:19:28 +0530

    update next js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,39 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+
+  images: {
+    domains: [],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  experimental: {
+    optimizeCss: false, // disable lightningcss
+  },
+
+  // Force Tailwind to use JS instead of native oxide
+  env: {
+    TAILWIND_MODE: "watch",
+    TAILWIND_DISABLE_NATIVE: "true",
+  },
 };
 
 export default nextConfig;
