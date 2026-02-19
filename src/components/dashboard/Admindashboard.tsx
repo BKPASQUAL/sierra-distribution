1bcdd39db2cc310d40f9faf8b1c8240d7b71f8b0
Bawantha <bawanthapasqual@gmail.com>
2026-02-19 22:27:12 +0530

    test

--- src/components/dashboard/Admindashboard.tsx
+++ src/components/dashboard/Admindashboard.tsx
@@ -591,7 +591,7 @@ export default function AdminDashboard() {
                     </div>
                   </div>
                 ))}
-              </div>
+              </div> 
             )}
           </CardContent>
         </Card>
